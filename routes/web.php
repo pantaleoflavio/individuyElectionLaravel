7e6deec274f6b8e1883e631b2a8ee93234166cca
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-08-13 11:37:21 +0200

    add the routes,for each crud operations:  for index, for auth, user, ranking, votes, wrestler, tag team, feds; user and vote routes are handeled with custom middleware; some user routes have to be implemented

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,58 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VoteController;
+use App\Http\Controllers\RankingController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\TagTeamController;
+use App\Http\Controllers\WrestlerController;
+use App\Http\Controllers\FederationController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Middleware\RedirectIfNotAuthenticated;
 
 Route::get('/', function () {
-    return view('components.layout');
+    return view('index');
 });
+
+// Auth Routes
+Route::middleware('guest')->group(function(){
+    Route::get('/register', [RegisteredUserController::class, 'create']);
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+
+    Route::get('/login', [SessionController::class, 'create']);
+    Route::post('/login', [SessionController::class, 'store']);
+});
+Route::delete('/logout', [SessionController::class, 'destroy'])->middleware('auth');
+
+// User Routes
+Route::middleware(['auth.custom'])->group(function () {
+    // Route::get('/profile/edit', [UserController::class, 'edit'])->name('user.edit');
+    // Route::put('/profile', [UserController::class, 'update'])->name('user.update');
+    Route::get('/profile', [UserController::class, 'show'])->name('user.profile');
+});
+
+// Ranking Routes
+Route::get('/vote-lists', [RankingController::class, 'index']);
+Route::get('/ranking-list-wrestler', [RankingController::class, 'ranking_list_wrestler']);
+Route::get('/ranking-list-tag-team', [RankingController::class, 'ranking_list_tag_team']);
+
+// Vote Routes
+Route::middleware(['auth.custom'])->group(function () {
+    Route::get('/voteWrestler/{wrestler}/{ranking}', [VoteController::class, 'showWrestlerVoteForm'])->name('vote.wrestler.form');
+    Route::post('/voteWrestler', [VoteController::class, 'wrestlerVoteStore'])->name('vote.wrestler.store');
+    Route::get('/voteTagTeam/{tagTeam}/{ranking}', [VoteController::class, 'showTagTeamVoteForm'])->name('vote.tagTeam.form');
+    Route::post('/voteTagTeam', [VoteController::class, 'tagTeamVoteStore'])->name('vote.tagTeam.store');
+});
+
+// Wrestler Routes
+Route::get('/wrestler-candidates', [WrestlerController::class, 'candidates']);
+
+// Tag Team Routes
+Route::get('/tag-team-candidates', [TagTeamController::class, 'candidates']);
+
+
+// Feds Routes
+Route::get('/federations', [FederationController::class, 'index']);
+Route::get('/list-per-fed&{id}', [FederationController::class, 'show'])->name('federations.show');;
