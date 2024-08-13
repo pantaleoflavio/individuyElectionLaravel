0a24adca85f2571a28c8ef5a7d38b3147487b5f4
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-08-13 13:50:52 +0200

    controllers for the user login and signup, with related crud operation

---
+++ app/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Validation\Rules\Password;
+
+class RegisteredUserController extends Controller
+{
+        /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $userAttributes = $request->validate([
+            'name' => ['required'],
+            'username' => ['required', 'unique:users,username'], // Validazione per username
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed', Password::min(6)],
+            'image' => ['nullable', 'image', 'max:2048'],
+        ]);
+
+        if ($request->hasFile('image')) {
+            $userAttributes['image_path'] = $request->file('image')->store('profile_images', 'public');
+        }
+
+        $user = User::create($userAttributes);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
