14d39d4e4490451eb4d2c58d529ce5ff24ffcd90
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-08-13 17:13:47 +0200

    custome middleware that redirect if not authenticate and registration of the middleware in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->appendToGroup('auth.custom', \App\Http\Middleware\RedirectIfNotAuthenticated::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
