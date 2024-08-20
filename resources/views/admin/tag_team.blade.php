9d86426f1d8d15aa81f2ec27e7e23ad415360ac6
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-08-20 15:28:26 +0200

    update

--- resources/views/admin/tag_team.blade.php
+++ resources/views/admin/tag_team.blade.php
@@ -1,33 +1,31 @@
 <x-admin-layout>
     <!-- Tag Team List-->
-    <div class="row">
-        <div class="col-md-12">
-            <h2>Lista Tag Team</h2>
-            <div class="d-flex justify-content-end">
-                <a href="{{ route('admin.tag_team.add') }}" class="btn btn-success">Aggiungi nuovo Tag Team</a>
-            </div>
-            <x-table id="candidatesTable">
-                <x-slot name="header">
-                    <x-table-header />
-                </x-slot>
-                @foreach($tagTeams as $tagTeam)
-                    <x-table-row :item="$tagTeam" url="{{ route('admin.tag_team.edit', $tagTeam->id) }}">
-                        Modifica
-                        @if($tagTeam->is_active)
-                            <td>in attivita</td>
-                        @else
-                            <td>ritirato</td>
-                        @endif
-                        <form method="POST" action="{{ route('admin.tag_team.delete', $tagTeam->id) }}" data-confirm="true">
-                            @csrf
-                            @method('DELETE')
-                            <td><button type="submit" class="btn btn-danger">Elimina</button></td>
-                        </form>
-                    </x-table-row>
 
-                @endforeach
-            </x-table>
-        </div>
+    <h2>Lista Tag Team</h2>
+    <div class="d-flex justify-content-end">
+        <a href="{{ route('admin.tag_team.add') }}" class="btn btn-success">Aggiungi nuovo Tag Team</a>
     </div>
+    <x-table id="candidatesTable">
+        <x-slot name="header">
+            <x-table-header />
+        </x-slot>
+        @foreach($tagTeams as $tagTeam)
+            <x-table-row :item="$tagTeam" url="{{ route('admin.tag_team.edit', $tagTeam->id) }}">
+                Modifica
+                @if($tagTeam->is_active)
+                    <td>in attivita</td>
+                @else
+                    <td>ritirato</td>
+                @endif
+                <form method="POST" action="{{ route('admin.tag_team.delete', $tagTeam->id) }}" data-confirm="true">
+                    @csrf
+                    @method('DELETE')
+                    <td><button type="submit" class="btn btn-danger">Elimina</button></td>
+                </form>
+            </x-table-row>
+
+        @endforeach
+    </x-table>
+
 
 </x-admin-layout>
--- resources/views/admin/wrestler.blade.php
+++ resources/views/admin/wrestler.blade.php
@@ -1,33 +1,30 @@
 <x-admin-layout>
     <!-- Wrestler List-->
-    <div class="row">
-        <div class="col-md-12">
-            <h2>Lista Wrestler</h2>
-            <div class="d-flex justify-content-end">
-                <a href="{{ route('admin.wrestler.add') }}" class="btn btn-success">Aggiungi nuovo Wrestler</a>
-            </div>
-            <x-table id="candidatesTable">
-                <x-slot name="header">
-                    <x-table-header />
-                </x-slot>
-                @foreach($wrestlers as $wrestler)
-                    <x-table-row :item="$wrestler" url="{{ route('admin.wrestler.edit', $wrestler->id) }}">
-                        Modifica
-                        @if($wrestler->is_active)
-                            <td>in attivita</td>
-                        @else
-                            <td>ritirato</td>
-                        @endif
-                        <form method="POST" action="{{ route('admin.wrestler.delete', $wrestler->id) }}" data-confirm="true">
-                            @csrf
-                            @method('DELETE')
-                            <td><button type="submit" class="btn btn-danger">Elimina</button></td>
-                        </form>
-                    </x-table-row>
 
-                @endforeach
-            </x-table>
-        </div>
+    <h2>Lista Wrestler</h2>
+    <div class="d-flex justify-content-end">
+        <a href="{{ route('admin.wrestler.add') }}" class="btn btn-success">Aggiungi nuovo Wrestler</a>
     </div>
+    <x-table id="candidatesTable">
+        <x-slot name="header">
+            <x-table-header />
+        </x-slot>
+        @foreach($wrestlers as $wrestler)
+            <x-table-row :item="$wrestler" url="{{ route('admin.wrestler.edit', $wrestler->id) }}">
+                Modifica
+                @if($wrestler->is_active)
+                    <td>in attivita</td>
+                @else
+                    <td>ritirato</td>
+                @endif
+                <form method="POST" action="{{ route('admin.wrestler.delete', $wrestler->id) }}" data-confirm="true">
+                    @csrf
+                    @method('DELETE')
+                    <td><button type="submit" class="btn btn-danger">Elimina</button></td>
+                </form>
+            </x-table-row>
+
+        @endforeach
+    </x-table>
 
 </x-admin-layout>
