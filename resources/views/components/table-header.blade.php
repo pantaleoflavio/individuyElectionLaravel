4c6f8e63a157426ccedf610d50c38df18aea7cf5
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-08-21 17:30:13 +0200

    update style components

--- resources/views/components/table-header.blade.php
+++ resources/views/components/table-header.blade.php
@@ -3,5 +3,5 @@
     <th data-sort="category">Stile <i class="fa-solid" id="icon-category"></i></th>
     <th data-sort="country">Paese <i class="fa-solid" id="icon-country"></i></th>
     <th data-sort="federation">Federazione <i class="fa-solid" id="icon-federation"></i></th>
-    <th class="">Azioni</th>
+    <th class=""></th>
 </tr>
