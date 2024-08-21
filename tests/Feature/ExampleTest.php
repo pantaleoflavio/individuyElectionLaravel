a45b824a993b0ff195bf0e3ee716434c32f9dfde
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-08-21 16:21:47 +0200

    fix

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -8,12 +8,12 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+    * A basic test example.
+    * public function test_the_application_returns_a_successful_response(): void
+    * {
+    *    $response = $this->get('/');
+ 
+    *         $response->assertStatus(200);
+    *    }
      */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
 }
