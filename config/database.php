0893683594d3fe9434bf9455e78b6936b0418f70
flaviopantaleo <flavio.pantaleo@yahoo.com>
2025-01-13 11:12:27 +0100

    update workflow

--- config/database.php
+++ config/database.php
@@ -65,7 +65,7 @@ return [
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
