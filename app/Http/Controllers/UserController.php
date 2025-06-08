c6f9e001a77dc4d1453066d4dc2306fd92ddb4e3
flaviopantaleo <flavio.pantaleo@yahoo.com>
2025-06-08 12:16:07 +0200

    Add PHPDoc

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,6 +40,7 @@ class UserController extends Controller
      */
     public function update(Request $request)
     {
+        /** @var User $user */
         $user = Auth::user();
 
         $request->validate([
