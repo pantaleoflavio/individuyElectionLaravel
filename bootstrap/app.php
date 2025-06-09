be6a4b1f5914f4114533be5dcc3a1d000384894e
flaviopantaleo <flavio.pantaleo@yahoo.com>
2025-06-09 13:10:44 +0200

    scheduling the calculate rankings command and update readme

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,20 +3,31 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Artisan;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Per il middleware custom auth
+        // Middleware custom per autenticazione
         $middleware->appendToGroup('auth.custom', \App\Http\Middleware\RedirectIfNotAuthenticated::class);
-        // Per il middleware admin
+        // Middleware per ruoli admin
         $middleware->appendToGroup('admin', \App\Http\Middleware\AdminMiddleware::class);
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule
+            ->command('app:calculate-all-ranking-averages')
+            ->weekly()
+            ->at('00:00')
+            ->name('weekly-log')
+            ->withoutOverlapping()
+            ->appendOutputTo(storage_path('logs/scheduler.log'));
+    })
+    ->create();
